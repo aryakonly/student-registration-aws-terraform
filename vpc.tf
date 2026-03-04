f6f2ee814242706734837e677bbd606cebad3454
Aryakadam <kadam.arya9058@gmail.com>
2026-03-04 10:59:03 +0530

    avalibility zone added

--- vpc.tf
+++ vpc.tf
@@ -11,6 +11,7 @@ resource "aws_subnet" "mysubnet-1" {
   vpc_id = "vpc-0bba4a05381dfa9f9"
   cidr_block = "192.78.0.0/20"
   map_public_ip_on_launch = true
+  availability_zone = "ap-south-1a"
   tags = {
     Name = "public-sunbet"
   }
@@ -19,6 +20,7 @@ resource "aws_subnet" "mysubnet-1" {
 resource "aws_subnet" "mysubnet-2" {
   vpc_id = "vpc-0bba4a05381dfa9f9"
   cidr_block = "192.78.16.0/20"
+  availability_zone = "ap-south-1b"
   tags = {
     Name = "private-sunbet"
   }
